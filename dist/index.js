77ba39126ee9c03bd1b931def80fab1c45b4d080
David Lee Perish <david@stonyhillsoftware.com>
2021-09-26 00:58:28 -0400

    Build for npm pkg

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+exports.__esModule = true;
+exports.Guid = void 0;
+var Guid_1 = require("./dist/Guid");
+__createBinding(exports, Guid_1, "Guid");
